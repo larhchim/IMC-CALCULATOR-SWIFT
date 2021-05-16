f675edde4318c80678102446e486e3e46a4e4128
LARHCHIM ISMAIL <larhchimismail@Mac-de-LARHCHIM.local>
2021-05-16 17:46:25 +0100

    first version

---
+++ IMC CALCULATOR/IMC.swift
@@ -0,0 +1,77 @@
+//
+//  IMC.swift
+//  IMC CALCULATOR
+//
+//  Created by LARHCHIM ISMAIL on 5/16/21.
+//  Copyright © 2021 LARHCHIM ISMAIL. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class IMC {
+    
+    var taille : Float
+    
+    var poids : Float
+    
+    var evaluation : Float {
+        
+        get {
+            return poids/pow(taille, 2)
+        }
+        
+    }
+    
+    var remarque : String {
+        get {
+            return calculer().0
+        }
+    }
+    
+    var couleur : UIColor {
+        get {
+            return calculer().1
+        }
+    }
+    
+    
+    init( taille : Float, poid : Float) {
+         
+        self.taille = taille
+        self.poids = poid
+        
+     }
+    
+    func calculer() ->(String,UIColor){
+        
+        var evaluation : Float {
+            
+            get {
+                return poids/pow(taille, 2)
+            }
+            
+        }
+        
+         switch evaluation {
+              
+               case  18,5..<25:
+                return ("Poids normale: votre IMC =\(String(format: "%.2f",evaluation))",.green)
+               case 25..<30:
+                return ("Surpoids: votre IMC =\(String(format: "%.2f",evaluation))",.yellow)
+               case 30..<35:
+                return("Obésité modérée: votre IMC =\(String(format: "%.2f",evaluation))",.cyan)
+               case 35..<40:
+                return("Obésité sévère: votre IMC =\(String(format: "%.2f",evaluation))",.red)
+               case 40..<200:
+                return ("Obésité massive: votre IMC =\(String(format: "%.2f",evaluation))",.brown)
+               default:
+                   return("Données incorrectes",UIColor.black)
+               }
+        
+    }
+    
+
+}
+
+
---
+++ IMC CALCULATOR/ViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController.swift
+//  IMC CALCULATOR
+//
+//  Created by LARHCHIM ISMAIL on 5/16/21.
+//  Copyright © 2021 LARHCHIM ISMAIL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var screen: UIView!
+    
+    @IBOutlet weak var resultat: UILabel!
+    
+   
+    @IBOutlet weak var TailleLabel: UILabel!
+    
+    
+    
+    @IBOutlet weak var poidLabel: UILabel!
+    
+    
+    @IBOutlet weak var TailleSlider: UISlider!
+    
+    
+    @IBOutlet weak var PoidSlider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func TailleAction(_ sender: UISlider) {
+        
+        TailleLabel.text = String(format: "%.2f",sender.value) + " m"
+        
+        TailleSlider.value = Float(String(format: "%.2f",sender.value))!
+      
+
+        
+    }
+    
+    
+    @IBAction func PoidAction(_ sender: UISlider) {
+        
+        poidLabel.text = String(format: "%.2f",sender.value) + " kg"
+        
+        PoidSlider.value = Float(String(format: "%.2f",sender.value))!
+        
+    }
+    
+    @IBAction func IMCAction(_ sender: Any) {
+        
+        //screen.backgroundColor = UIColor.red
+        //print("taille est", TailleSlider.value)
+        //print("poid est", PoidSlider.value)
+        let calculatrice = IMC(taille: TailleSlider.value, poid: TailleSlider.value)
+        
+        screen.backgroundColor = calculatrice.calculer().1
+        
+        resultat.text = calculatrice.calculer().0
+        
+        
+    }
+    
+    
+    @IBAction func retry(_ sender: Any) {
+        
+        screen.backgroundColor = UIColor.white
+        resultat.text = "Resultat"
+    }
+    
+}
+
